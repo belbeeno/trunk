56d3857f8fe52c7a2d0143cc5d4a21ba08649745
Timo Vink <timovink@gmail.com>
2016-04-06 20:04:15 -0700

    Update GenerationOptions to match scale of trunk.

--- Assets/Scripts/City/Generation/GenerationOptions.cs
+++ Assets/Scripts/City/Generation/GenerationOptions.cs
@@ -7,13 +7,13 @@ public class GenerationOptions
     [Header("City Size")]
     [Range(1, 32)] public int blocksWidth;
     [Range(1, 32)] public int blocksHeight;
-    [Range(4f, 8f)] public float blockSize;
+    [Range(10f, 200f)] public float blockSize;
     public float cityWidth { get { return blocksWidth * blockSize; } }
     public float cityHeight { get { return blocksHeight * blockSize; } }
     
     [Header("Building Size")]
     [Range(0f, 1f)] public float roadWidth;
-    [Range(0f, 8f)] public float floorHeight;
+    [Range(10f, 100f)] public float floorHeight;
     
     [Header("River")]
     [Range(0.5f, 2f)] public float riverWidth;
