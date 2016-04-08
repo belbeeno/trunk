8317dbbea0520be33010a179a9f9217f30143111
belbeeno <das_garage@hotmail.com>
2016-04-07 22:24:06 -0700

    Points of Interest
    Getting 'Points of Interest' in, with a skeleton of how it works as part
    of the City prefab.  May want to get this working with the city
    generator?

---
+++ Assets/Scripts/UI/OperatorItems/POI.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class POI : MonoBehaviour
+{
+    public LayerMask targetMask;
+
+    public Text textfield = null;
+    public Image background = null;
+
+    public RectTransform rect = null;
+    private RectTransform parentRect = null;
+	
+    void Start()
+    {
+        if (rect == null)
+        {
+            rect = GetComponent<RectTransform>();
+        }
+        parentRect = rect.parent.GetComponent<RectTransform>();
+        textfield.text = "";
+        background.enabled = false;
+    }
+
+    public float maxMoveDelta = 10f;
+
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector2 outPos;
+        bool isOnScreen = RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRect, Input.mousePosition, Camera.main, out outPos);
+        if (isOnScreen)
+        {
+            rect.anchoredPosition = Vector2.MoveTowards(rect.anchoredPosition, outPos, maxMoveDelta);
+        }
+        else
+        {
+            return;
+        }
+
+        Ray camToCity = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit info;
+        bool success = false;
+        if (Physics.Raycast(camToCity, out info, 1000f, targetMask.value))
+        {
+            if (info.collider != null && info.collider.tag.Equals("POI"))
+            {
+                string target = info.collider.gameObject.name;
+                textfield.text = target.Substring(0, Mathf.Min(textfield.text.Length + 1, target.Length));
+                success = true;
+            }
+        }
+
+        if (!success)
+        {
+            textfield.text = (textfield.text.Length <= 1 ? string.Empty : textfield.text.Substring(0, textfield.text.Length - 1));
+        }
+
+        background.enabled = (textfield.text.Length > 0);
+
+	}
+}
