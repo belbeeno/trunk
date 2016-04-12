c452b9bcb00235db66493eb6fa8bb04f6b517765
Timo Vink <timovink@gmail.com>
2016-04-11 20:42:26 -0700

    Add GameManager to setup and start game.

--- Assets/Scripts/City/GameObjects/City.cs
+++ Assets/Scripts/City/GameObjects/City.cs
@@ -1,43 +1,14 @@
-using System.Linq;
 using UnityEngine;
 
 public class City : MonoBehaviour 
 {
-    public GenerationOptions generationOptions;
-    
-    private CityGenerator _generator = new CityGenerator();
-    
-	void Start () 
+    public void GenerateGeometry(GenerationResult result)
     {    
-    }
-	
-	void Update () 
-    {
-	}
-    
-    public void Generate()
-    {
         while (transform.childCount > 0) 
         {
             GameObject.DestroyImmediate(transform.GetChild(0).gameObject);
         }
-        
-        var result = _generator.Generate(generationOptions);
-        InitializeRoutePlanner(result);
-        CreateGameObjects(result);
-        RepositionCamera();
-        StartCar();
-    }
-    
-    private void InitializeRoutePlanner(GenerationResult result)
-    {
-        var gameObj = GameObject.Find("RoutePlanner");
-        var routePlanner = gameObj.GetComponent<RoutePlanner>();
-        routePlanner.graph = result.roadGraph;
-    }
-    
-    private void CreateGameObjects(GenerationResult result)
-    {     
+                 
         // Roads
         var roadsObj = new GameObject("Roads");
         roadsObj.transform.parent = this.transform;
@@ -67,21 +38,5 @@ public class City : MonoBehaviour
         riverObj.transform.parent = this.transform;
         var riverScript = riverObj.AddComponent<River>();
         riverScript.riverGraph = result.riverGraph;
-    }
-    
-    private void RepositionCamera()
-    {
-        var cameraX =  generationOptions.cityWidth / 2f;
-        var cameraZ = generationOptions.cityHeight / 2f; 
-        Camera.main.transform.position = new Vector3(cameraX, 300f, cameraZ);
-        Camera.main.transform.rotation = Quaternion.LookRotation(Vector3.down, Vector3.forward);
-        Camera.main.orthographicSize = generationOptions.cityHeight / 2f;
-    }
-    
-    private void StartCar()
-    {
-        var gameObj = GameObject.Find("Car");
-        var car = gameObj.GetComponent<TrunkMover>();
-        car.canMove = true;
     }
 }
