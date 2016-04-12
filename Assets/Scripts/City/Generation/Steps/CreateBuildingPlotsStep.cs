a2f7eb11104499b692b5864bc49553d2e97128ba
Timo Vink <timovink@gmail.com>
2016-04-11 19:55:50 -0700

    Inset buildings from plot edge.

--- Assets/Scripts/City/Generation/Steps/CreateBuildingPlotsStep.cs
+++ Assets/Scripts/City/Generation/Steps/CreateBuildingPlotsStep.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -13,13 +14,32 @@ public class CreateBuildingPlotsStep : GenerationStepBase
         data.buildingPlots = plots;
     }
     
-    public BuildingPlotData CreateBuildingPlot(CityBlockData city)
+    private BuildingPlotData CreateBuildingPlot(CityBlockData city)
     {
-        var corners = city.boundingRoads.Select(r => r.from.pos).ToArray();
+        var corners = GetInsetCorners(city.boundingRoads);
         var numFloors = Random.Range(1, 6);
         var floorHeight = options.blockSize * options.floorHeight;
         var buildingPlot = new BuildingPlotData(corners, numFloors, floorHeight);
         
         return buildingPlot;
+    }
+    
+    private Vector3[] GetInsetCorners(RoadEdge[] edges)
+    {
+        // Inset boundaries
+        var insetAmount = (options.roadWidth / 2f) * options.blockSize;
+        var lines = edges.Select(e => Line.CreateThroughPoints(e.from.pos, e.to.pos));
+        var insetLines = lines.Select(l => l.Offset(l.Perpendicular() * insetAmount));
+        
+        // Find intersections
+        var corners = new List<Vector3>();
+        var prevLine = insetLines.Last();
+        foreach (var line in insetLines)
+        {
+            corners.Add(Line.Intersection(prevLine, line));
+            prevLine = line;    
+        }
+
+        return corners.ToArray();
     }
 }
