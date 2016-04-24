1912c1f6b73138e365fb210f49d23460594a1716
belbeeno <das_garage@hotmail.com>
2016-04-24 01:35:53 -0700

    Operator UI
    Getting work done to prepare for the phone states.
    Gonna take a nap...

--- Assets/Scripts/UI/AppWindow.cs
+++ Assets/Scripts/UI/AppWindow.cs
@@ -7,14 +7,37 @@ public class AppWindow : MonoBehaviour
     [System.Serializable]
     public class OnActiveChangedCB : UnityEvent<bool> { }
     public OnActiveChangedCB OnActiveChanged;
-	
+
+    private AudioSource _audio = null;
+    public AudioSource Audio
+    {
+        get
+        {
+            if (_audio == null)
+                _audio = transform.parent.GetComponent<AudioSource>();
+            return _audio;
+        }
+    }
+    public AudioClip onEnable = null;
+    public AudioClip onDisable = null;
+
     void OnEnable()
     {
+        if (Audio && onEnable != null)
+        {
+            Audio.clip = onEnable;
+            Audio.Play();
+        }
         OnActiveChanged.Invoke(true);
     }
 
     void OnDisable()
     {
+        if (Audio && onDisable != null)
+        {
+            Audio.clip = onDisable;
+            Audio.Play();
+        }
         OnActiveChanged.Invoke(false);
     }
 
